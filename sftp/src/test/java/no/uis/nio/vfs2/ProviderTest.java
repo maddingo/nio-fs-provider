7fda22f73123bdeba7aac59e00571d21ef7bfd32
Martin Goldhahn <martin.goldhahn@uis.no>
2012-10-03 09:30:10 +0200

    test cases

--- sftp/src/test/java/no/uis/nio/vfs2/ProviderTest.java
+++ sftp/src/test/java/no/uis/nio/vfs2/ProviderTest.java
@@ -1,36 +1,22 @@
 package no.uis.nio.vfs2;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import java.net.URI;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 
 
 public class ProviderTest {
   
   @Test
-  public void testNewFileSystem() throws Exception {
+  public void testgetSftpUriPath() throws Exception {
 
     URI uri = URI.create("sftp://lportal-test.uis.no/");
-    Map<String, String> env = new HashMap<String, String>();
-    try (FileSystem ftpFs = FileSystems.newFileSystem(uri, env)) {
-      assertThat(ftpFs, is(notNullValue()));
-    }
-  }
-  
-  @Test
-  public void testOpenFile() throws Exception {
-    URI uri = URI.create("sftp://lportal-test.uis.no/");
-    Map<String, String> env = new HashMap<String, String>();
-    try (FileSystem ftpFs = FileSystems.newFileSystem(uri, env)) {
-      assertThat(ftpFs, is(notNullValue()));
-      assertThat(ftpFs.isOpen(), is(true));
-    }
+    Path sftpPath = Paths.get(uri);
+    assertThat(sftpPath, is(notNullValue()));
   }
 }
