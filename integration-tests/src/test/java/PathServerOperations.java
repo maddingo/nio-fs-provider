e28bf15204bb01bd263ae63d33a78d25cdda0257
Martin Goldhahn <mgoldhahn@gmail.com>
2023-03-25 13:42:47 +0100

    restore integration tests

---
+++ integration-tests/src/test/java/PathServerOperations.java
@@ -0,0 +1,24 @@
+import org.hamcrest.Matcher;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.hamcrest.Matchers.is;
+
+public class PathServerOperations {
+
+    public static Stream<Arguments> data() {
+        return Stream.of(
+            Arguments.of("sftp://localhost/", is(true)),
+            Arguments.of("sftp://localhost/testfile/", is(true))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("data")
+    private void startSftpServer(String uriString, Matcher<?> expected) {
+
+    }
+}
