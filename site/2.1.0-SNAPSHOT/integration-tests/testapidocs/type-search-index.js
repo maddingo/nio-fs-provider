795b0c7dd80745ec773e4a456b9ac4d280bb6b8d
Martin Goldhahn <mgoldhahn@gmail.com>
2024-12-27 17:13:48 +0100

    Site checkin for project nio-fs

---
+++ site/2.1.0-SNAPSHOT/integration-tests/testapidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"no.maddin.niofs.it","l":"FileOperationsIT"},{"p":"no.maddin.niofs.it","l":"FilesListTest"},{"p":"no.maddin.niofs.it","l":"PathsGetUriIT"}];updateSearchResults();
---
+++ site/2.1.0-SNAPSHOT/nio-fs-sftp/testapidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"no.maddin.niofs.sftp","l":"FileSystemsTest"},{"p":"no.maddin.niofs.sftp","l":"PartsTest"},{"p":"no.maddin.niofs.sftp","l":"PathTest"}];updateSearchResults();
